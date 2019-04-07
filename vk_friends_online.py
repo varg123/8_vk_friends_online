6ebdc3cac6106d803a57d0aae2bf7b261f4c288f
Andrey Skilev <andreyskilev@mail.ru>
2019-04-07 09:13:35 +0500

    Написал решение задачи

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,15 +1,7 @@
 import vk
+from getpass import getpass
 
-
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
-
-
-def get_user_login():
-    pass
-
-
-def get_user_password():
-    pass
+APP_ID = -1
 
 
 def get_online_friends(login, password):
@@ -18,15 +10,36 @@ def get_online_friends(login, password):
         user_login=login,
         user_password=password,
     )
-    api = vk.API(session)
-    # например, api.friends.get()
+    api = vk.API(
+        session,
+        v='5.35',
+    )
+    fields = [
+        'first_name',
+        'last_name',
+        'online'
+    ]
+    friends = api.friends.get(fields=fields)
+    return list(filter(lambda x: x['online'], friends['items']))
 
 
 def output_friends_to_console(friends_online):
-    pass
+    count_friends = len(friends_online)
+    print(f"Друзья в онлайне {count_friends}:")
+    for friend in friends_online:
+        first_name = friend['first_name']
+        last_name = friend['last_name']
+        print(f"{last_name} {first_name}")
+
+
+def main():
+    login = input("Введите логин:")
+    password = getpass("Введите пароль:")
+    try:
+        friends_online = get_online_friends(login, password)
+    except vk.exceptions.VkAuthError:
+        exit()
+    output_friends_to_console(friends_online)
 
 if __name__ == '__main__':
-    login = get_user_login()
-    password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    main()
